09c1ddd28c08bef19ccc041b4e23868dd4c43d72
jbkim08 <jbkim08@gmail.com>
2022-12-06 09:41:06 +0900

    빈객체 등록옵션

--- BeanCreate/src/main/java/com/demo/main/MainClass.java
+++ BeanCreate/src/main/java/com/demo/main/MainClass.java
@@ -12,9 +12,23 @@ public class MainClass {
 				new ClassPathXmlApplicationContext("com/demo/config/beans.xml");
 	
 		//id가 없으므로 클래스의 타입만 가지고 객체를 가져옴
-		TestBean t0 = ctx.getBean(TestBean.class);
+		TestBean t0 = ctx.getBean("test0", TestBean.class);
 		System.out.printf("t0 : %s\n", t0);
-
+		//id가 있을때 id로 가져옴 
+		TestBean t1 = ctx.getBean("test1",TestBean.class);
+		System.out.printf("t1 : %s\n", t1);		
+		//id가 있을때 id로 가져옴 
+		TestBean t2 = ctx.getBean("test1",TestBean.class);
+		System.out.printf("t2 : %s\n", t2);
+		//lazy-init은 객체를 가져올때 생성한다.
+		TestBean t3 = ctx.getBean("test2",TestBean.class);
+		System.out.printf("t3 : %s\n", t3);
+		
+		//scope=prototype은 가져올때마다 생성
+		TestBean t4 = ctx.getBean("test3",TestBean.class);
+		System.out.printf("t4 : %s\n", t4);
+		TestBean t5 = ctx.getBean("test3",TestBean.class);
+		System.out.printf("t5 : %s\n", t5);
 		
 		ctx.close();
 	}	
